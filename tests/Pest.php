c270269a5a951d69936f009b59b0855e8993a598
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-06-04 15:42:19 +0200

    add formats

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,22 +4,48 @@ define('MP3_NO_META', __DIR__.'/media/test-no-meta.mp3');
 define('AUDIOBOOK', __DIR__.'/media/audiobook.m4b');
 define('MD', __DIR__.'/media/test.md');
 
+define('ALAC', __DIR__.'/media/test-alac.m4a');
 define('AAC', __DIR__.'/media/test.aac');
+define('AIF', __DIR__.'/media/test.aif');
+define('AIFC', __DIR__.'/media/test.aifc');
+define('AIFF', __DIR__.'/media/test.aiff');
 define('FLAC', __DIR__.'/media/test.flac');
 define('M4A', __DIR__.'/media/test.m4a');
 define('M4B', __DIR__.'/media/test.m4b');
+define('M4V', __DIR__.'/media/test.m4v');
+define('MKA', __DIR__.'/media/test.mka');
+define('MKV', __DIR__.'/media/test.mkv');
 define('MP3', __DIR__.'/media/test.mp3');
 define('MP4', __DIR__.'/media/test.mp4');
 define('OGG', __DIR__.'/media/test.ogg');
+define('OPUS', __DIR__.'/media/test.opus');
+define('SPX', __DIR__.'/media/test.spx');
+define('TTA', __DIR__.'/media/test.tta');
 define('WAV', __DIR__.'/media/test.wav');
+define('WEBM', __DIR__.'/media/test.webm');
 define('WMA', __DIR__.'/media/test.wma');
+define('WV', __DIR__.'/media/test.wv');
 
 define('AUDIO', [
+    // ALAC,
+    // AAC,
+    // AIF,
+    // AIFC,
+    // AIFF,
     FLAC,
     M4A,
     M4B,
+    // M4V,
+    // MKA,
+    // MKV,
     MP3,
     MP4,
+    // OGG,
+    // OPUS,
+    // SPX,
+    // TTA,
     WAV,
+    // WEBM,
     WMA,
+    // WV,
 ]);
